42342eae2dc1c77e2af7b3d9ce76e657e2cd6c4c
VineelaYemireddy <vinee.reddy.96@gmail.com>
2021-06-23 08:30:06 +1000

    loopsPartTwo.js

---
+++ 03Intermediate/13loopsPartTwo.js
@@ -0,0 +1,11 @@
+const myStates = [
+	"Rajasthan",
+	"Delhi",
+	"Assam",
+	1947,
+	"Tamil Nadu",
+	"Maharashtra",
+];
+
+myStates.forEach((s) => console.log(s));
+myStates.forEach((s) => console.log(s));
